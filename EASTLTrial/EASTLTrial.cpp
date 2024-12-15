cc0df5b1a9131fe01fb28f24813b27741ad70c81
ktetsuo <ktetsuo@users.noreply.github.com>
2024-12-16 00:12:23 +0900

    add 2 projects

---
+++ EASTLTrial/EASTLTrial.cpp
@@ -0,0 +1,19 @@
+﻿#include <EASTL/fixed_vector.h>
+#include <iostream>
+
+int main()
+{
+    eastl::fixed_vector<int, 5, false> myVector;
+    myVector.push_back(1);
+    myVector.push_back(2);
+    myVector.push_back(3);
+    myVector.push_back(4);
+    myVector.push_back(5);
+
+    for(int i : myVector)
+    {
+        std::cout << i << std::endl;
+    }
+
+    return 0;
+}
---
+++ ETLTrial/ETLTrial.cpp
@@ -0,0 +1,18 @@
+﻿#include <etl/vector.h>
+#include <iostream>
+
+int main()
+{
+    etl::vector<int, 5> myVector;
+	myVector.push_back(1);
+	myVector.push_back(2);
+    myVector.push_back(3);
+    myVector.push_back(4);
+    myVector.push_back(5);
+
+    for(int i : myVector)
+    {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
